7d0b1830802585cb63d86d51bdb5547540c89320
Adrien Chaussabel <adrien.chaussabel@gmail.com>
2021-04-23 17:55:48 -0700

    added a menu text script for battle

---
+++ Assets/MenuFlow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFlow : MonoBehaviour
+{
+
+    public TMPro.TextMeshProUGUI[] texts;
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (i < texts.Length - 1)
+        {
+            if(Input.GetKeyDown("space"))
+            {
+                Debug.Log("Spaced pressed");
+                texts[i].enabled = false;
+                i++;
+                texts[i].enabled = true;
+            }
+        }
+    }
+}
